8b7e4037aaff0b710144a40077c452ff25df777a
ancion <ancion.github.com>
2024-06-12 07:22:51 +0800

    update

---
+++ eww/scripts/b.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+hyprpm list | awk '
+  BEGIN { b="start" } 
+  {
+    if($0~/Plugin \w+/){ b=$4; a[b]=0}; 
+      if($0~/enabled: /){ a[b]=$3}
+  }
+  END { 
+    printf "["
+    i=0;
+    for (key in a) {
+      i++; 
+      printf "{" 
+      printf "\"name\":" "\""key"\", \"value\": " (index(a[key], "true") > 1 ? "true" : "false")
+      if (length(a) == i) {
+        printf "}"
+      }else {
+        printf "},"
+      }
+    }
+    printf "]"
+  }'
