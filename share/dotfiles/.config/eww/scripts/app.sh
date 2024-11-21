fc1b0106292937cb4df65d2a584842620a1ebc03
ancion <ancion@github.com>
2024-11-21 20:01:08 +0800

    update

--- share/dotfiles/.config/eww/scripts/app.sh
+++ share/dotfiles/.config/eww/scripts/app.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 if [[ "$1" == "--welcome" ]]; then
-   APPIMAGELAUNCHER_DISABLE=1 $HOME/.config/ml4w/apps/ML4W_Welcome-x86_64.AppImage &
+   com.ml4w.welcome &
 elif [[ "$1" == "--dotfiles" ]]; then
-   APPIMAGELAUNCHER_DISABLE=1 $HOME/.config/ml4w/apps/ML4W_Dotfiles_Settings-x86_64.AppImage &
+   com.ml4w.dotfilessettings &
 elif [[ "$1" == "--hyprland" ]]; then
-   APPIMAGELAUNCHER_DISABLE=1 $HOME/.config/ml4w/apps/ML4W_Hyprland_Settings-x86_64.AppImage &
+   com.ml4w.hyprland.settings &
 else
     echo "ERROR: $1 not found"
 fi
