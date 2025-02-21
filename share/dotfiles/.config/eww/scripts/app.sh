33528be74438352fce352a7fdb3b13edeedf91d5
ancion <ancion@github.com>
2025-02-21 22:30:22 +0800

    update

--- share/dotfiles/.config/eww/scripts/app.sh
+++ share/dotfiles/.config/eww/scripts/app.sh
@@ -2,7 +2,7 @@
 if [[ "$1" == "--welcome" ]]; then
    com.ml4w.welcome &
 elif [[ "$1" == "--dotfiles" ]]; then
-   com.ml4w.dotfilessettings &
+   com.ml4w.settings &
 elif [[ "$1" == "--hyprland" ]]; then
    com.ml4w.hyprland.settings &
 else
