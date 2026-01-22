095b7f19de2255ec771a57e781958e3ed794907c
ancion <ancion@github.com>
2026-01-22 19:25:10 +0800

    update

--- dotfiles/.config/nwg-dock-hyprland/launch.sh
+++ dotfiles/.config/nwg-dock-hyprland/launch.sh
@@ -16,9 +16,9 @@ if [ ! -f $HOME/.config/ml4w/settings/dock-disabled ]; then
     sleep 0.5
     # nwg-dock-hyprland -i 90 -p bottom -ico awn-applet -mb 5 -d -s style.css -c "/home/unicorn/.config/eww/scripts/applaucher.sh"
     if [ -f $HOME/.config/ml4w/settings/dock-autohide ]; then
-        nwg-dock-hyprland -d -i 32 -w 5 -mb 10 -x -s themes/$DOCK_THEME/style.css -c "$HOME/.config/hypr/scripts/launcher.sh"
+        nwg-dock-hyprland -d -i 90 -w 5 -mb 2 -x -c "$HOME/.config/eww/scripts/applaucher.sh"
     else
-        nwg-dock-hyprland -i 32 -w 5 -mb 10 -x -s themes/$DOCK_THEME/style.css -c "$HOME/.config/hypr/scripts/launcher.sh"
+        nwg-dock-hyprland -i 90 -w 5 -mb 2 -x -c "$HOME/.config/eww/scripts/applaucher.sh"
     fi
 else
     echo ":: Dock disabled"
