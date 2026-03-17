8154752da866af6e5527cd2e48a205863b0ab490
ancion <ancion@github.com>
2026-03-17 18:08:19 +0800

    update

--- dotfiles/.config/eww/scripts/curscreen.sh
+++ dotfiles/.config/eww/scripts/curscreen.sh
@@ -1,3 +1,10 @@
-#screen=0
-screen=$(hyprctl activewindow -j | jq '.monitor')
+# get current screen
+screen=0
+
+if [ $XDG_CURRENT_DESKTOP == "Hyrpland" ]; then
+  screen=$(hyprctl activewindow -j | jq '.monitor')
+elif [ $XDG_CURRENT_DESKTOP == "niri" ]; then
+  niri msg focused-output | grep HDMI >/dev/null && screen=1 || screen=0
+fi
+
 echo $screen
